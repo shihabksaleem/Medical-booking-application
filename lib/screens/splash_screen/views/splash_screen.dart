19c15c15e8457053e2df910a6cd153649fdf8de4
shihabksaleem <shihab.ks56@gmail.com>
2023-04-26 13:31:32 +0530

    home screen added

--- lib/screens/splash_screen/views/splash_screen.dart
+++ lib/screens/splash_screen/views/splash_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:medical_booking_application/utils/color_constants.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    TextStyle style1 = TextStyle(color: ColorConstant.mainBlack);
     return Scaffold(
       body: Container(
+        color: ColorConstant.mainWhite,
         child: Center(
-          child: Text('Logo'),
+          child: Text(
+            'Logo',
+            style: style1,
+          ),
         ),
       ),
     );
